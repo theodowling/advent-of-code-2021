9fc5a7b500a95623d68b1573ad041d9dd722b654
Theo Dowling <thdowling@gmail.com>
2021-12-01 13:22:07 +0000

    Day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,70 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  @spec part1(any) :: integer
+  @doc """
+
+      iex> part1(\"""
+      ...> 199
+      ...> 200
+      ...> 208
+      ...> 210
+      ...> 200
+      ...> 207
+      ...> 240
+      ...> 269
+      ...> 260
+      ...> 263
+      ...> \""")
+      7
+  """
+  def part1(args) do
+    {count, _} =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer(&1))
+      |> Enum.reduce({0, 9999}, fn n, {acc, prev} ->
+        acc =
+          if n > prev do
+            acc + 1
+          else
+            acc
+          end
+
+        {acc, n}
+      end)
+
+    count
   end
 
-  def part2(_args) do
+  @doc """
+
+      iex> part2(\"""
+      ...> 199
+      ...> 200
+      ...> 208
+      ...> 210
+      ...> 200
+      ...> 207
+      ...> 240
+      ...> 269
+      ...> 260
+      ...> 263
+      ...> \""")
+      5
+  """
+
+  def part2(args) do
+    [a, b, c | rest] =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer(&1))
+
+    Enum.reduce(rest, {{a, b, c}, 0}, fn n, {{a, b, c}, acc} ->
+      if a + b + c < b + c + n do
+        {{b, c, n}, acc + 1}
+      else
+        {{b, c, n}, acc}
+      end
+    end)
+    |> elem(1)
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -2,20 +2,5 @@ defmodule AdventOfCode.Day01Test do
   use ExUnit.Case
 
   import AdventOfCode.Day01
-
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
-  end
-
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
-  end
+  doctest AdventOfCode.Day01
 end
