42c1e89a857711d0ec9e47e6fe8ca7c3e6a7e509
Theo Dowling <thdowling@gmail.com>
2021-12-17 11:16:24 +0200

    Cleanup

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -50,7 +50,7 @@ defmodule AdventOfCode.Day15 do
 
     n = Enum.count(lines)
 
-    maps =
+    grid =
       lines
       |> Enum.with_index(1)
       |> Enum.map(fn {line, y} ->
@@ -59,6 +59,7 @@ defmodule AdventOfCode.Day15 do
         |> Enum.map(&String.to_integer(&1))
         |> Enum.with_index(1)
         |> Enum.map(fn {v, x} ->
+          # Grow 5 times
           for m1 <- 0..4,
               m2 <- 0..4 do
             {{x + m1 * n, y + m2 * n}, wrap_around(v, m1, m2)}
@@ -78,46 +79,19 @@ defmodule AdventOfCode.Day15 do
         end
       )
 
-    graph =
-      maps
-      |> Enum.reduce(graph, fn {a, v}, acc ->
-        a
-        |> neighbours(n, n)
-        |> Enum.reduce(acc, fn b, acc ->
-          Graph.add_edge(acc, a, b, weight: v)
-        end)
+    grid
+    |> Enum.reduce(graph, fn {a, v}, acc ->
+      a
+      |> neighbours(n, n)
+      |> Enum.reduce(acc, fn b, acc ->
+        Graph.add_edge(acc, a, b, weight: v)
       end)
-
-    # graph =
-    #   graph
-    #   |> Graph.edges()
-    #   |> Enum.reduce(graph, fn %Graph.Edge{v1: {x1, y1}, v2: {x2, y2}}, acc ->
-    #     if x1 != x2 and y1 != y2 do
-    #       Graph.delete_edge(acc, {x1, y1}, {x2, y2})
-    #     else
-    #       acc
-    #     end
-    #   end)
-
-    # |> IO.inspect()
-
-    # IO.inspect(Graph.out_edges(graph, {69, 18}), label: "out edges")
-    # IO.inspect(Graph.in_edges(graph, {332, 404}), label: "in edges")
-
-    IO.inspect(graph)
-
-    graph
+    end)
     |> Graph.dijkstra({1, 1}, {n, n})
-    # |> tap(fn x -> Enum.each(x, &IO.puts(inspect(&1))) end)
-    |> IO.inspect()
     |> Enum.filter(fn a -> a != {1, 1} end)
-    |> Enum.map(&Map.get(maps, &1))
+    |> Enum.map(&Map.get(grid, &1))
     |> Enum.sum()
   end
-
-  # 1190 is too low
-  # 1325
-  # 2874 is answer for someone else
 
   def wrap_around(v, m1, m2) do
     k = v + m1 + m2
